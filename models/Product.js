e8f995a2f87efd3fc2537eccffd2bd072899adc9
kuldeepSingh <singhkuldeep7850@gmail.com>
2023-03-08 17:23:49 +0530

    first commit

---
+++ models/Product.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+const ProductSchema = new mongoose.Schema({
+    name:String,
+    price:Number,
+    description:String,
+    category:String,
+    rating:Number,
+    supply:Number
+})
+
+export default mongoose.models.Product || mongoose.model("Product", ProductSchema)
---
+++ models/User.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        min:2,
+        max:100
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        min:5
+    },
+    city:String,
+    state:String,
+    country:String,
+    occupation:String,
+    phoneNumber:String,
+    transactions:Array,
+    role:{
+        type:String,
+        enum:["user","admin","superadmin"],
+        default:"admin"
+    }
+},{ timestamps:true })
+
+
+export default mongoose.models.User || mongoose.model("User", UserSchema)
